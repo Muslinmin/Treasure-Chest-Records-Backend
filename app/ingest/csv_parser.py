93d3837cdf9899c2b5be2784485ca861f8562772
lalawee <crusader2adventureabjure@gmail.com>
2026-05-19 14:21:21 +0800

    normalized inputs from csv file for database

---
+++ app/ingest/csv_parser.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+import csv
+
+
+"""
+transactions
+  transaction_date DATE     NOT NULL
+  amount_cents     INTEGER  NOT NULL        ← $12.50 stored as 1250
+  description      TEXT
+  transaction_code TEXT                     ← nullable, bank's internal code
+  ref1             TEXT                     ← vendor name, nullable
+  is_settled       BOOLEAN  NOT NULL        ← True/False only
+  category         TEXT                     ← nullable, filled later
+  is_category_manual BOOLEAN DEFAULT FALSE
+  source_file      TEXT                     ← which CSV this came from
+
+"""
+
+
+from datetime import datetime
+
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+def parse_csv(filepath: Path) -> list[dict]:
+    csv_file = Path(filepath)
+    records = list()
+    with csv_file.open(mode="r", encoding="utf-8") as f:
+        for line in f:
+            if line.startswith("Transaction Date"):
+                headers = next(csv.reader([line]))
+                break
+        reader = csv.DictReader(f, fieldnames=headers)
+        for row in reader:
+            row_dict = {} #initialize a new row dictionary for clean tracking below:
+            if not row["Transaction Date"]:
+                continue
+            else:
+                dt_object = datetime.strptime(row["Transaction Date"], "%d-%b-%y")
+                row_dict["transaction_date"] = dt_object.date()
+
+                amount_cents = int()
+                if row["Debit Amount"]:
+                    amount_cents = int(float(row.pop("Debit Amount")) * 100 * -1)
+                    del row["Credit Amount"]
+                elif row["Credit Amount"]:
+                    amount_cents = int(float(row.pop("Credit Amount")) * 100)
+                    del row["Debit Amount"]
+                else:
+                    logger.warning(f"Row skipped — no debit or credit amount: {row}")
+                    # @todo how to handle rows with no amount in both credit and debit?
+                    continue
+                row_dict["amount_cents"] = amount_cents
+
+                row_dict["description"] = row["Description"]
+
+                row_dict["transaction_code"] = row["Transaction Code"]
+
+                row_dict["ref1"] = row["Transaction Ref1"]
+
+                settled_status = row["Status"].lower()
+
+                if settled_status == "settled":
+                    row_dict["is_settled"] = True
+                else:
+                    row_dict["is_settled"] = False
+
+                row_dict["category"] = None
+
+                row_dict["is_category_manual"] = None
+                
+
+                file_name = os.path.basename(filepath)
+                row_dict["source_file"] = file_name
+            
+                records.append(row_dict)
+    if not records:
+        raise Exception(f"Records are empty ! {records}")
+    return records
--- app/ingest/test_parser.py
+++ app/ingest/test_parser.py
@@ -1,25 +1,41 @@
 from pathlib import Path
 import pytest
-from parser import parse_csv 
+from csv_parser import parse_csv 
 import datetime
 
 def test_parse_csv_retrieves_output():
     file_path = r"C:\Users\lordm\OneDrive\Desktop\Projects\Treasure-Chest-Records-Backend\data\inbox\test.csv"
     result = parse_csv(file_path)
-    assert len(result) == 2
+    assert len(result) == 3
     
-    assert result[0]["Transaction Date"] == datetime.date(2026, 5, 8)
-    assert result[0]["Debit Amount"] == "13.2"
-    assert result[0]["Description"] == "SUBWAY @ SMU SINGAPORE SGP 06MAY xxxx-xxxx-xxxx-xxxx 000002391139531"
+    assert result[0]["transaction_date"] == datetime.date(2026, 5, 8)
+    assert result[0]["description"] == "SUBWAY @ SMU SINGAPORE SGP 06MAY xxxx-xxxx-xxxx-xxxx 000002391139531"
     
-    assert result[1]["Transaction Date"] == datetime.date(2026, 5, 8)
-    assert result[1]["Debit Amount"] == "3.4"
-    assert result[1]["Description"] == "NTUC FairPrice App Pay SI SGP 05MAY 5264-7110-2059-4505 000002394032280"
+    assert result[1]["transaction_date"] == datetime.date(2026, 5, 8)
+    assert result[1]["description"] == "NTUC FairPrice App Pay SI SGP 05MAY 5264-7110-2059-4505 000002394032280"
 
+    assert result[2]["transaction_date"] == datetime.date(2026, 6, 12)
+    assert result[2]["description"] == "Payment from Boss"
 
+def test_amount_cents_retrieved():
+    file_path = r"C:\Users\lordm\OneDrive\Desktop\Projects\Treasure-Chest-Records-Backend\data\inbox\test.csv"
+    result = parse_csv(file_path)
+    assert len(result) == 3
+    assert result[0]["amount_cents"] == -1320
+    assert "Debit Amount" not in result[0]
+    assert "Credit Amount" not in result[0]
+    assert result[1]["amount_cents"] == -340
+    assert "Debit Amount" not in result[1]
+    assert "Credit Amount" not in result[1]
+    assert result[2]["amount_cents"] == 15000
+    assert "Debit Amount" not in result[2]
+    assert "Credit Amount" not in result[2]
 
 def test_parse_csv_raises_exception_if_empty():
     file_path = r"C:\Users\lordm\OneDrive\Desktop\Projects\Treasure-Chest-Records-Backend\data\inbox\test_exec.csv"
     with pytest.raises(Exception) as exec_info:
             parse_csv(file_path)
             assert("Records are empty" in exec_info.value)
+
+
+
